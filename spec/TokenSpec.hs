f18ebb8504521b4379c20c5a6a84ecd067899d8d
Ashok Menon <amenon94@gmail.com>
2015-10-07 23:12:27 +0100

    Spec: Token left/right associativity tests

--- spec/TokenSpec.hs
+++ spec/TokenSpec.hs
@@ -13,7 +13,17 @@ spec = do
       (plusP, multP) `shouldSatisfy` uncurry (<)
   describe "fixPrec" $ do
     it "binds * tighter than +" $ do
-       let t1 = Op "*" (Op "+" (Leaf 1) (Leaf 2)) (Leaf 3) :: OpTree Int
-       let t2 = Op "+" (Leaf 1) (Op "*" (Leaf 2) (Leaf 3)) :: OpTree Int
-       fixPrec t1 `shouldBe` t2
-       fixPrec t2 `shouldBe` t2
+      let t1 = Op "*" (Op "+" (Leaf 1) (Leaf 2)) (Leaf 3) :: OpTree Int
+      let t2 = Op "+" (Leaf 1) (Op "*" (Leaf 2) (Leaf 3)) :: OpTree Int
+      fixPrec t1 `shouldBe` t2
+      fixPrec t2 `shouldBe` t2
+    context "when priority is the same" $ do
+      context "and the operator is left associative" $ do
+        it "performs no rotation" $ do
+          let t = Op "+" (Op "+" (Leaf 1) (Leaf 2)) (Leaf 3)
+          fixPrec t `shouldBe` t
+      context "and the operator is right associative" $ do
+        it "performs the rotation" $ do
+          let t1 = Op ":" (Op ":" (Leaf 1) (Leaf 2)) (Leaf 3)
+          let t2 = Op ":" (Leaf 1) (Op ":" (Leaf 2) (Leaf 3))
+          fixPrec t1 `shouldBe` t2
