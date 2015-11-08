d99feba7ad9212db6f90afc5d967bfc4a39dc08f
Ashok Menon <amenon94@gmail.com>
2015-11-08 23:05:40 +0000

    DynArray: Implementation of Dynamic arrays
    Dynamically resizable arrays will be used in the implementation of type contexts.

---
+++ spec/DynArraySpec.hs
@@ -0,0 +1,50 @@
+module DynArraySpec where
+
+import SpecHelper
+import Control.Monad
+import Control.Monad.ST
+import DynArray
+
+spec :: Spec
+spec = do
+  describe "push" $ do
+    it "adds elements" $ do
+      let res = runST $ do { d <- newArray_ 10
+                           ; push d 1
+                           ; peek d
+                           }
+      (res :: Int) `shouldBe` 1
+
+  describe "pop" $ do
+    it "removes elements" $ do
+      let res = runST $ do { d <- newArray_ 4
+                           ; forM_ [1..3] (push d)
+                           ; pop d
+                           ; peek d
+                           }
+      (res :: Int) `shouldBe` 2
+
+  describe "fetch" $ do
+   it "accesses random elements" $ do
+     let res = runST $ do { d <- newArray_ 10
+                          ; forM_ [1..10] (push d)
+                          ; forM  [0..9]  (fetch d)
+                          }
+     (res :: [Int]) `shouldBe` [1..10]
+
+  describe "expansion" $ do
+    it "preserves contents" $ do
+      let res = runST $ do { d <- newArray_ 1
+                           ; forM_ ([1, 2]) (push d)
+                           ; replicateM 2 (pop' d)
+                           }
+      (res :: [Int]) `shouldBe` [2, 1]
+
+  describe "compaction" $ do
+    it "preserves contents" $ do
+      let res = runST $ do { d <- newArray_ 8
+                           ; forM_ [1..8] (push d)
+                           ; replicateM_ 7 (pop d)
+                           ; peek d
+                           }
+      (res :: Int) `shouldBe` 1
---
+++ src/DynArray.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+{-|
+Mutable, resizable STArrays with integer indices.
+Interface includes both random access and a stack interface.
+|-}
+module DynArray ( DynArray
+                , newArray_
+                , fetch
+                , peek
+                , pop'
+                , pop
+                , push
+                ) where
+
+import Control.Monad
+import Control.Monad.ST
+import qualified Data.Array.ST as STA
+import Data.STRef
+
+data DArray s e = DArray { buf  :: STA.STArray s Int e
+                         , size :: !Int
+                         , cap  :: !Int
+                         }
+
+newtype DynArray s e = DA (STRef s (DArray s e))
+
+-- | Create a new Dynamic Array.
+newArray_ :: Int
+          -- ^ Initial Capacity
+          -> ST s (DynArray s e)
+
+newArray_ c = do
+  arr <- STA.newArray_ (0, c - 1)
+  ref <- newSTRef $ DArray { buf = arr, size = 0, cap = c }
+  return $ DA ref
+
+-- | Array Random Access
+fetch :: DynArray s e -> Int -> ST s e
+fetch (DA ref) i = do
+  arr <- readSTRef ref
+  STA.readArray (buf arr) i
+
+-- | Access the last element in the array.
+peek :: DynArray s e -> ST s e
+peek (DA ref) = do
+  DArray {buf, size} <- readSTRef ref
+  STA.readArray buf (size - 1)
+
+-- | Remove the last element in the array. This could cause the array to become
+-- compacted.
+pop :: DynArray s e -> ST s ()
+pop (DA ref) = do
+  modifySTRef ref decSize
+  compact ref
+  where
+    decSize d@DArray{size = s} = d{size = 0 `max` s-1}
+
+-- | Variant of @ pop @ that returns the last element as well as removing it.
+pop' :: DynArray s e -> ST s e
+pop' d = do { x <- peek d; pop d; return x }
+
+-- | Append an element to the end of the array. This could trigger an array
+-- expansion.
+push :: DynArray s e -> e -> ST s ()
+push (DA ref) x = do
+  makeRoom ref
+  DArray {buf, size} <- readSTRef ref
+  STA.writeArray buf size x
+  modifySTRef ref incSize
+  where
+    incSize d@DArray{size = s} = d{size = s+1}
+
+compact, makeRoom :: STRef s (DArray s e) -> ST s ()
+
+compact ref = do
+  DArray {buf, size, cap} <- readSTRef ref
+  when (size < cap `div` 4) $ do
+    let cap' = cap `div` 2
+    buf' <- resize buf cap'
+    writeSTRef ref $ DArray {buf = buf', cap = cap', size}
+
+makeRoom ref = do
+  DArray {buf, size, cap} <- readSTRef ref
+  when (size >= cap) $ do
+    let cap' = 2 * cap
+    buf' <- resize buf cap'
+    writeSTRef ref $ DArray {buf = buf', cap = cap', size}
+
+resize :: STA.STArray s Int e -> Int -> ST s (STA.STArray s Int e)
+resize from cap = STA.getElems from >>= STA.newListArray (0, cap - 1)
