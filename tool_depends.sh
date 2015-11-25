03c6fe53514b55dba4d6ba5c7fa8b638e28e3ead
Ashok Menon <amenon94@gmail.com>
2015-11-25 13:20:25 +0000

    Lexer: Alex updated on hackage.
     * Reinstating the sandbox, and updating the Alex dependency to the
       fixed version.

--- tool_depends.sh
+++ tool_depends.sh
@@ -1,12 +1,5 @@
 #! /bin/sh
 # Run me before you setup the sandbox.
 
-cabal install 'happy == 1.19.*' --global
-
-git submodule update --recursive
-cd alex
-cabal install quickcheck --global
-runhaskell Setup.lhs configure
-runhaskell Setup.lhs build
-runhaskell Setup.lhs install
-cd -
+cabal --require-sandbox install 'alex-3.1.5'
+cabal --require-sandbox install 'happy == 1.19.*'
