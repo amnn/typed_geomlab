63cee8974ca553be6e5a5bed39f50a9bd890f9b7
Ashok Menon <amenon94@gmail.com>
2015-12-02 12:55:15 +0000

    Main: Adding option flags
    Options are interleaved between files, are turned on by +xyz for flags
    xyz and turned off by -xyz. Flags will act on all files processed after
    they are turned on. By default the only flag thats turned on is the type
    checking flag.

--- src/Main.hs
+++ src/Main.hs
@@ -1,39 +1,97 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as BS
-import Control.Monad ()
+import Control.Monad.State
 import Desugar (desugarExpr)
+import qualified Expr as E (stripLoc)
 import GLParser (parseExpr)
 import Infer (typeCheck)
 import Lexer
-import Sugar
+import Sugar hiding (stripLoc)
+import qualified Sugar as S (stripLoc)
 import System.Console.ANSI
 import System.Environment
 import TyError
 
-runAndFormat :: Show a => (a -> IO ()) -> Alex a -> BS.ByteString -> IO ()
-runAndFormat fmt am input = either putStrLn fmt (runAlex input am)
+data Opt = Opt { showRaw   :: !Bool
+               , showSugar :: !Bool
+               , showExpr  :: !Bool
+               , showLoc   :: !Bool
+               , showType  :: !Bool
+               }
 
-inGreen :: IO () -> IO ()
-inGreen io = setSGR [SetColor Foreground Dull Green] >> io >> setSGR []
+defaultOpts :: Opt
+defaultOpts = Opt { showRaw   = False
+                  , showSugar = False
+                  , showExpr  = False
+                  , showLoc   = False
+                  , showType  = True
+                  }
 
-tc :: FilePath -> BS.ByteString -> IO ()
-tc fname input = runAndFormat (mapM_ fmt) tcM input
+setOpt :: Bool -> Char -> StateT Opt IO ()
+setOpt b o = do
+  case o of
+    'r' -> modify' $ \opt -> opt{showRaw   = b}
+    's' -> modify' $ \opt -> opt{showSugar = b}
+    'e' -> modify' $ \opt -> opt{showExpr  = b}
+    'l' -> modify' $ \opt -> opt{showLoc   = b}
+    't' -> modify' $ \opt -> opt{showType  = b}
+    _   -> return ()
+
+fmt :: [SGR] -> IO () -> StateT Opt IO ()
+fmt sgr io = liftIO (setSGR sgr >> io >> setSGR [])
+
+inRed, inGreen, inBold, inFaint :: IO () -> StateT Opt IO ()
+inRed   = fmt [SetColor Foreground Dull Red]
+inGreen = fmt [SetColor Foreground Dull Green]
+inBold  = fmt [SetConsoleIntensity BoldIntensity]
+inFaint = fmt [SetConsoleIntensity FaintIntensity]
+
+processArg :: String -> StateT Opt IO ()
+
+processArg ('+':opts) = mapM_ (setOpt True)  opts
+processArg ('-':opts) = mapM_ (setOpt False) opts
+
+processArg fname = do
+  inBold $ putStrLn ("\n" ++ fname ++ "\n")
+
+  Opt{showRaw, showSugar, showExpr, showLoc, showType} <- get
+
+  -- Read File
+  raw <- liftIO $ BS.readFile fname
+  when showRaw $ do
+    inFaint $ putStrLn "File Contents\n"
+    liftIO $ BS.putStrLn raw >> putStrLn ""
+
+  -- Parsing
+  case runAlex raw parseExpr of
+    (Left e)      -> inRed $ putStrLn e
+    (Right sugar) -> do
+      when showSugar $ do
+        inFaint $ putStrLn "Parsed\n"
+        liftIO $ if showLoc
+        then mapM_ print sugar >> putStrLn ""
+        else mapM_ (print . fmap S.stripLoc) sugar >> putStrLn ""
+
+      -- Desugaring
+      let expr  = map (fmap desugarExpr) sugar
+      when showExpr $ do
+        inFaint $ putStrLn "Desugared\n"
+        liftIO $ if showLoc
+        then mapM_ print expr >> putStrLn ""
+        else mapM_ (print . fmap E.stripLoc) expr >> putStrLn ""
+
+      -- Type checking
+      when showType $ do
+        inFaint $ putStrLn "Type Checked\n"
+        let types = typeCheck expr
+        mapM_ (disp raw) types
   where
-    tcM                   = typeCheck . map (fmap desugarExpr) <$> parseExpr
-
-    fmt (Def x (Right t)) = inGreen (putStrLn $ x ++ " :: " ++ show t)
-    fmt (Def _ (Left e))  = printError fname input e
-    fmt (Eval  (Right t)) = inGreen (putStrLn $ show t)
-    fmt (Eval  (Left e))  = printError fname input e
-
-processFile :: String -> IO ()
-processFile fname = do
-  putStrLn ("\n*** Checking " ++ fname ++ "\n")
-  input <- BS.readFile fname
-  tc fname input
+    disp _   (Def x (Right t)) = inGreen (putStrLn $ x ++ " :: " ++ show t)
+    disp raw (Def _ (Left e))  = liftIO $ printError fname raw e
+    disp _   (Eval  (Right t)) = inGreen (putStrLn $ show t)
+    disp raw (Eval  (Left e))  = liftIO $ printError fname raw e
 
 main :: IO ()
-main = do
-  files <- getArgs
-  mapM_ processFile files
+main = evalStateT (liftIO getArgs >>= mapM_ processArg) defaultOpts
