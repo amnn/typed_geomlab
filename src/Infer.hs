c8a85ecd0157a226b20ae22ca17ce23d3d3b1486
Ashok Menon <amenon94@gmail.com>
2016-05-10 18:40:06 +0100

    [infer] Factoring out common monadic operations.

--- src/Infer.hs
+++ src/Infer.hs
@@ -23,46 +23,13 @@ import           Data.Monad.DynArray
 import           Data.Monad.State
 import           Data.Monad.Type
 import           Data.Patt
-import           Data.STRef
 import           Data.Structure         (shapeEq)
 import           Data.Sugar
 import           Data.Token             (Id)
 import           Data.TyError
 import           Data.Type
 import           Debug.Trace            (traceM)
-
--- | Constraint of all the Monads used by the type checker.
-type MonadInfer m = ( MonadST m
-                    , MonadReader (ScopedState (World m)) m
-                    , MonadError TyError m
-                    )
-
--- The usual STRef operations, lifted to any monad that supports ST operations.
-
-newIRef :: MonadST m => a -> m (STRef (World m) a)
-newIRef = liftST . newSTRef
-
-readIRef :: MonadST m => STRef (World m) a -> m a
-readIRef = liftST . readSTRef
-
-writeIRef :: MonadST m => STRef (World m) a -> a -> m ()
-writeIRef sr x = liftST (writeSTRef sr x)
-
-modifyIRef :: MonadST m => STRef (World m) a -> (a -> a) -> m ()
-modifyIRef sr f = liftST (modifySTRef sr f)
-
--- | Query the environment for the current level.
-getCurrLvl :: MonadInfer m => m Int
-getCurrLvl = asks lvl
-
--- | Query the global state for the list of local variables.
-getTyCtx :: MonadInfer m => m (DynArray (World m) (TyRef (World m)))
-getTyCtx = tyCtx <$> (asks gsRef >>= readIRef)
-
--- | Query the environment for the type of a particular local variable,
--- identified by its deBruijn index.
-getLocalTy :: MonadInfer m => Int -> m (TyRef (World m))
-getLocalTy ix = getTyCtx >>= liftST . peek ix
+import           Infer.Monad
 
 -- | Introduce a new local variable to the stack, and return a reference to it.
 pushLocal :: MonadInfer m => m (TyRef (World m))
@@ -75,11 +42,6 @@ pushLocal = do
 -- | Remove the top-most local variable from the stack.
 popLocal :: MonadInfer m => m ()
 popLocal = getTyCtx >>= liftST . pop
-
--- | Run the supplied monad in a scope nested one level below the current one.
-newScope :: MonadInfer m => m a -> m a
-newScope = local bumpScope
-  where bumpScope st@SS {lvl = l} = st{lvl = l + 1}
 
 -- | Produce a unique variable name.
 fresh :: MonadInfer m => m Id
@@ -506,14 +468,6 @@ initialDefs = H.fromList <$> mapM absDef ts
          , ("_debug",  ArrT [] BoolT)
          , ("_print",  ArrT [StrT] (ListT (VarT "a")))
          ]
-
--- | Concrete Monad Transformer Stack satisfying the @ MonadInfer @
--- constraint. @ StateT (GloDef s) @ is introduced so as to keep track of the
--- global definition map without storing a reference to it.
-type InferM s = ReaderT (ScopedState s)
-              ( ExceptT TyError
-              ( StateT (GloDef s)
-              ( ST s)))
 
 -- | Given a list of top-level statements, type check each one individually and
 -- give the type or error for each top-level operation.
---
+++ src/Infer/Monad.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE ConstraintKinds  #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+{-|
+
+Shape of the Monad that type inference runs in, and a suite of basic operations
+it provides.
+
+|-}
+module Infer.Monad where
+
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Control.Monad.ST
+import           Control.Monad.ST.Class
+import           Control.Monad.State
+import           Data.Monad.DynArray
+import           Data.Monad.State
+import           Data.Monad.Type
+import           Data.STRef
+import           Data.TyError
+
+-- | Constraint of all the Monads used by the type checker.
+type MonadInfer m = ( MonadST m
+                    , MonadReader (ScopedState (World m)) m
+                    , MonadError TyError m
+                    )
+
+-- | Concrete Monad Transformer Stack satisfying the @ MonadInfer @
+-- constraint. @ StateT (GloDef s) @ is introduced so as to keep track of the
+-- global definition map without storing a reference to it.
+type InferM s = ReaderT (ScopedState s)
+              ( ExceptT TyError
+              ( StateT (GloDef s)
+              ( ST s)))
+
+-- The usual STRef operations, lifted to any monad that supports ST operations.
+newIRef :: MonadST m => a -> m (STRef (World m) a)
+newIRef = liftST . newSTRef
+
+readIRef :: MonadST m => STRef (World m) a -> m a
+readIRef = liftST . readSTRef
+
+writeIRef :: MonadST m => STRef (World m) a -> a -> m ()
+writeIRef sr x = liftST (writeSTRef sr x)
+
+modifyIRef :: MonadST m => STRef (World m) a -> (a -> a) -> m ()
+modifyIRef sr f = liftST (modifySTRef sr f)
+
+-- | Query the environment for the current level.
+getCurrLvl :: MonadInfer m => m Int
+getCurrLvl = asks lvl
+
+-- | Query the global state for the list of local variables.
+getTyCtx :: MonadInfer m => m (DynArray (World m) (TyRef (World m)))
+getTyCtx = tyCtx <$> (asks gsRef >>= readIRef)
+
+-- | Query the environment for the type of a particular local variable,
+-- identified by its deBruijn index.
+getLocalTy :: MonadInfer m => Int -> m (TyRef (World m))
+getLocalTy ix = getTyCtx >>= liftST . peek ix
+
+-- | Run the supplied monad in a scope nested one level below the current one.
+newScope :: MonadInfer m => m a -> m a
+newScope = local bumpScope
+  where bumpScope st@SS {lvl = l} = st{lvl = l + 1}
