eb33930821ef22f821b15ce5eef28820d7809dc6
Ashok Menon <amenon94@gmail.com>
2016-05-10 23:00:25 +0100

    [infer] Factoring out generalisation and instantiation!

--- src/Infer.hs
+++ src/Infer.hs
@@ -24,6 +24,7 @@ import           Data.Sugar
 import           Data.Token             (Id)
 import           Data.TyError
 import           Data.Type
+import           Infer.Generalise
 import           Infer.Levels
 import           Infer.Monad
 import           Infer.TypeFactory
@@ -39,81 +40,6 @@ cycleFree tr = do
     VarTB (FwdV t) -> cycleFree t
     _ | Marked _ <- newLevel -> throwError . OccursE =<< resolveTyRef tr
     _ -> do afterMarking 0 tr $ mapM_ cycleFree ty
-
--- | Find all type references from lower scopes than the current one, and ensure
--- that level adjustments have all been made for them. This is done before a
--- generalisation, as a level adjustment could result in a type not being
--- generalised.
-forceDelayedAdjustments :: MonadInfer m => m ()
-forceDelayedAdjustments = do
-  SS {gsRef}             <- ask
-  gs@GS{waitingToAdjust} <- readIRef gsRef
-  delayed                <- foldM adjustTop [] waitingToAdjust
-  writeIRef gsRef gs{waitingToAdjust = delayed}
-  where
-    adjustTop ts tr = do
-      lvl <- Lvl <$> asks lvl
-      StratTy{ty, newLevel = Set nLvl, oldLevel = oLvl} <- readIRef tr
-      case ty of
-        _ | oLvl <= lvl  -> return (tr:ts)
-        _ | oLvl == nLvl -> return ts
-        _ -> do
-          Set mLvl <- markTy 0 tr
-          ts'  <- foldM (adjustRec nLvl) ts ty
-          unifyLevels tr mLvl
-          return ts'
-
-    adjustRec nLvl ts _tr = do
-      _tr <- repr _tr
-      StratTy{newLevel = mNLvl'} <- readIRef _tr
-      case mNLvl' of
-        Marked _  -> throwError . OccursE =<< resolveTyRef _tr
-        Set nLvl' -> do
-          when (nLvl' > nLvl) (setLevel _tr $ Set nLvl)
-          adjustTop ts _tr
-
--- | Generalisation involves universally quantifying variables that are scoped
--- strictly below the current level.
-generalise :: MonadInfer m => TyRef (World m) -> m ()
-generalise tr = do
-  forceDelayedAdjustments
-  gen tr
-  where
-    gen _ur = do
-      _ur <- repr _ur
-      lvl <- Lvl <$> asks lvl
-      StratTy{ty, newLevel = Set l} <- readIRef _ur
-      when (l > lvl) $
-        case ty of
-          VarTB (FreeV _)    -> setLevel _ur $ Set Gen
-          _ | length ty == 0 -> return ()
-            | otherwise -> do
-            reps <- mapM repr ty
-            mapM_ gen reps
-            lvls <- mapM getLevel reps
-            let maxLvl = maximum lvls
-            unifyLevels _ur maxLvl
-
--- | Replace universally quantified variables in a (possibly) general type with
--- fresh type variables at the current level: Every instance of a generalised
--- type is new, and unification with one instance should not affect another.
-instantiate :: MonadInfer m => TyRef (World m) -> m (TyRef (World m))
-instantiate tRef = evalStateT (inst tRef) H.empty
-  where
-    inst tr = do
-      StratTy{ty, newLevel = Set lvl} <- readIRef tr
-      case ty of
-        VarTB (FreeV n) | Gen <- lvl -> do
-          subst <- get
-          case H.lookup n subst of
-            Just ty' -> return ty'
-            Nothing  -> do
-              nr <- newVar
-              put (H.insert n nr subst)
-              return nr
-        VarTB (FwdV link) -> inst link
-        _ | Gen <- lvl    -> mapM inst ty >>= newTy
-        _                 -> return tr
 
 -- | Calculate the type of a given expression, given a map from free variables
 -- to type references. Alternatively, an error may be thrown.
